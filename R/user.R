a9baa4eca1a725da9e526f9c2b89c045a2a45c17
Hiroaki Yutani <yutani.ini@gmail.com>
2017-10-25 22:16:53 +0900

    fix document

--- R/user.R
+++ R/user.R
@@ -6,16 +6,16 @@
 #' @seealso \url{https://github.com/Medium/medium-api-docs#getting-the-authenticated-users-details}
 #' @return
 #'   * `id`: A unique identifier for the user.
-#'   * `username`: The user’s username on Medium.
-#'   * `name`: The user’s name on Medium.
-#'   * `url`: The URL to the user’s profile on Medium
-#'   * `imageUrl`: The URL to the user’s avatar on Medium
+#'   * `username`: The user's username on Medium.
+#'   * `name`: The user's name on Medium.
+#'   * `url`: The URL to the user's profile on Medium
+#'   * `imageUrl`: The URL to the user's avatar on Medium
 #' @export
 medium_get_current_user <- function() {
   result <- medium_request("GET", "/v1/me")
 
   # user ID is needed to send requests to Publications API and Posts API.
-  Sys.setenv("MEDIUM_USER_ID" = result$data$id)
+  Sys.setenv(MEDIUM_USER_ID = result$data$id)
 
   result$data
 }
